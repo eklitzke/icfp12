4cfbdd6a7a9d24c42f3f1461a2fe9c6efefa8fec
Brandon Bickford <bickfordb@gmail.com>
2012-07-13 10:22:03 -0700

    Add world

--- run.py
+++ run.py
@@ -1,18 +1,13 @@
 "A skeleton module for reading standard input"
 import argparse
-import fileinput
 import logging
-import sys
+import world
 
 opt_parser = argparse.ArgumentParser()
 opt_parser.add_argument('--verbose', '-v', dest='verbosity', default=0, action='count')
 opt_parser.add_argument('files', nargs='*')
 log = logging.getLogger('app')
 log_fmt = u"%(asctime)s %(process)s %(levelname)s %(name)s %(filename)s:%(lineno)s %(message)s"
-
-def on_line(line):
-  "Handle a line"
-  log.debug('got line: %r', line)
 
 def verbosity_to_log_level(verbosity):
   if verbosity >= 2:
@@ -27,8 +22,8 @@ def main():
   args = opt_parser.parse_args()
   logging.basicConfig(level=verbosity_to_log_level(args.verbosity), format=log_fmt)
   log.info('startup')
-  for line in fileinput.input(args.files):
-    on_line(line)
+  w = world.read_world(args.files)
+  log.debug('read world: %r', w)
   log.info('shutdown')
 
 if __name__ == "__main__":
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+${PYTHON:-python} run.py -v -v maps/contest1.map
