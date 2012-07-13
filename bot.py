96f8dd4c219c237f2d20f78108f599c4a946fa85
Rhett Garber <rhettg@gmail.com>
2012-07-13 14:52:23 -0700

    bot types

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import argparse
+import types
 import random
 import logging
 
@@ -8,7 +9,31 @@ log = logging.getLogger(__name__)
 
 class Bot(object):
     def pick_move(self, the_world):
+        raise NotImplementedError
+
+class RandomBot(object):
+    name = "random"
+    def pick_move(self, the_world):
         return random.choice(MOVE_COMMANDS)
+
+class WeightedBot(Bot):
+    name = "weighted"
+
+    DEFAULT_WEIGHT = 1.0
+    WEIGHTS = {'W': 0.5, 'A': 0.10}
+    def pick_move(self, the_world):
+        running_weight = 0
+        weighted_chooser = []
+        for move in MOVE_COMMANDS:
+            running_weight += self.WEIGHTS.get(move, self.DEFAULT_WEIGHT)
+            weighted_chooser.append((running_weight, move))
+
+        choice = random.random() * running_weight
+        ndx = 0
+        while weighted_chooser[ndx][0] < choice:
+            ndx +=1
+
+        return weighted_chooser[ndx][1]
 
 def run_bot(bot, base_world, iterations):
     max_score = -100000
@@ -37,11 +62,19 @@ def run_bot(bot, base_world, iterations):
 
     return best_world, max_score, max_moves
 
+def bot_for_name(name):
+    for cls in globals().values():
+        if type(cls) == type and getattr(cls, 'name', None) == name:
+            return cls()
+    else:
+        raise AttributeError(name)
+
 if __name__ == "__main__":
     import world
     opt_parser = argparse.ArgumentParser()
     #opt_parser.add_argument('--verbose', '-v', dest='verbosity', default=0, action='count')
     opt_parser.add_argument('--iterations', '-i', dest='iterations', default=1000, type=int)
+    opt_parser.add_argument('--name', '-n', dest='name', default="random")
     opt_parser.add_argument('file')
     args = opt_parser.parse_args()
 
@@ -49,7 +82,7 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG, format=log_fmt, filename="bot.log")
     log.debug("Starting vis")
 
-    the_bot = Bot()
+    the_bot = bot_for_name(args.name)
     the_world = world.read_world(args.file)
 
     world, score, moves = run_bot(the_bot, the_world, args.iterations)
